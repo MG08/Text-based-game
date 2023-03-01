859062b342b2d601e3e19abaf5b177c3be121d94
MG08 <126265520+MG08@users.noreply.github.com>
2023-03-01 09:57:34 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,91 @@
+room = 1
+choice = "hi"
+print("welcome to the text based game!")
+
+while choice != "quit": #game loop------------------------------------
+    if room == 1:
+        choice = input("You are in room 1. You can go East. ")
+        if choice == "East" or choice == "east":
+            room = 2
+        else:
+            print("I don't understand that.")
+            
+    elif room == 2:
+        choice = input("You are in room 2. You can go West, South, or North")
+        if choice == "West" or choice == "west":
+            room = 1
+        if choice == "South" or choice == "south":
+            room = 3
+        if choice == "North" or choice == "north":
+            room = 4
+        else:
+            print("I don't understand that.")
+            
+    elif room == 3:
+        choice = input("You are in room 3. You can go North or East. ")
+        if choice == "North" or choice == "north":
+            room = 2
+        if choice == "East" or choice == "east":
+            room = 5
+        else:
+            print("I dont understand that.")
+            
+    elif room == 4:
+        choice = input("You are in room 4. You can go South or East.")
+        if choice == "East" or choice == "east":
+            room = 6
+        if choice == "South" or choice == "south":
+            room = 2
+        else:
+            print("I don't understand that.")
+            
+    elif room == 5:
+        choice = input("You are in room 5. You can go West or East.")
+        if choice == "East" or choice == "east":
+            room = 8
+        if choice == "West" or choice == "west":
+            room = 3
+        else:
+            print("I don't understand that.")
+            
+    elif room == 6:
+        choice = input("You are in room 6. You can go West or East.")
+        if choice == "West" or choice == "west":
+            room = 4
+        if choice == "East" or choice == "east":
+            room = 7
+        else:
+            print("I don't understand that.")
+            
+    elif room == 7:
+        choice = input("You are in room 7. You can go West or South.")
+        if choice == "West" or choice == "west":
+            room = 6
+        if choice == "South" or choice == "south":
+            room = 10
+        else:
+            print("I don't understand that.")
+            
+    elif room == 8:
+        choice = input("You are in room 8. You can go West, North, or South.")
+        if choice == "West" or choice == "west":
+            room = 5
+        if choice == "South" or choice == "south":
+            room = 9
+        if choice == "North" or choice == "north":
+            room = 10
+        else:
+            print("I don't understand that.")
+            
+    elif room == 9:
+        choice = input("You are in room 9. You can go North.")
+        if choice == "North" or choice == "north":
+            room = 8
+        else:
+            print("I don't understand that.")
+            
+    elif room == 10:
+        choice = input("You are in room 10.")
+
+#end pf game loop
+print("Thanks for playing. Goodbye.")
