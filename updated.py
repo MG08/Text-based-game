7f6ff8ed7576f11fa5646df147b02de6a24319a1
MG08 <126265520+MG08@users.noreply.github.com>
2023-05-05 10:01:02 -0600

    Update updated.py

--- updated.py
+++ updated.py
@@ -11,15 +11,19 @@ while choice != "quit": #game loop------------------------------------
         if choice == "No" or choice == "no":
             print("You stay in the room, still followed with the ominous silence. Nothing out of the ordinary in this room. Only thing to do is going through the door.")
         else:
-            print("I don't understand that.")
+            print("Type something that makes sense")
             
     elif room == 2:
-        choice = input("You enter the room...There's nothing out of the ordinary in this room either, except those weird dark humanid looking statues beside each other... You can either enter the left door")
+        choice = input("You enter the room...There's nothing out of the ordinary in this room either, except those weird dark humanid looking statues beside each other... You can either enter the left door or the right door in this room. Which one do you want to enter?")
         if choice == "West" or choice == "west":
             room = 1
-        if choice == "South" or choice == "south":
+        if choice == "Right" or choice == "right":
+            if inventory[0] == "key":
+                print("The door unlocks, you go in...")
             room = 3
-        if choice == "North" or choice == "north":
+            elif inventory[0] != "key":
+                print("The door doesn't budge, it looks like it's locked.")
+        if choice == "Left" or choice == "left":
             room = 4
         else:
             print("I don't understand that.")
@@ -34,8 +38,8 @@ while choice != "quit": #game loop------------------------------------
             print("I dont understand that.")
             
     elif room == 4:
-        choice = input("You are in room 4. You can go South or East.")
-        if choice == "East" or choice == "east":
+        choice = input("You enter the room, a large clock on the left with a door on the right. There’s a window right in front of you. Enter the right door?")
+        if choice == "Right" or choice == "right":
             room = 6
         if choice == "South" or choice == "south":
             room = 2
